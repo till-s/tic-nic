80c9e1e0cfadee768a2ed7be89714c0e70690707
till straumann <till.straumann@alumni.tu-berlin.de>
2024-06-15 17:42:15 +0200

    added script to update git version package
    unfortunately EFX tool does not support scripting which
    could do this automatically :-(

---
+++ firmware/efx/update_git_version_pkg.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+here=`dirname $0`
+echo "running from $here"
+
+f="$here/../hdl/GitVersionPkg.vhd"
+ver="00000000"
+
+if git diff-index --quiet HEAD -- ; then
+  ver=`git rev-parse --short=8 HEAD`
+else
+  echo "HEAD seems to be dirty; using version 00000000"
+fi
+echo "-- AUTOMATICALLY GENERATED; DO NOT EDIT"                                 >  $f
+echo "library ieee;"                                                           >> $f
+echo "use ieee.std_logic_1164.all"                                             >> $f
+echo ""                                                                        >> $f
+echo "package GitVersionPkg is"                                                >> $f
+echo "   constant GIT_VERSION_C : std_logic_vector(31 downto 0) := x\"$ver\";" >> $f
+echo "end package GitVersionPkg;"                                              >> $f
